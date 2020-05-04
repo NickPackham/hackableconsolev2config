312bb5f01b11e558840d59888b50dd96c5406d58
Nick Packham <46549847+NickPackham@users.noreply.github.com>
2020-05-03 18:55:20 -0700

    Up

--- lua/autorun/voy_load_config.lua
+++ lua/autorun/voy_load_config.lua
@@ -1,4 +1,4 @@
-hook.Add("Initialize", "Initialize_Voy_Console", function()
+hook.Add("DarkRPFinishedLoading", "Initialize_Voy_Console", function()
 	include("voy_console_config/config.lua")
 	AddCSLuaFile("voy_console_config/config.lua")
 end)
