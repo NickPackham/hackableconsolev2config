3ce4a43154321abafcc202c404c7e54728fa68ba
Nick Packham <46549847+NickPackham@users.noreply.github.com>
2020-05-04 17:52:58 -0700

    Update config.lua

--- lua/voy_console_config/config.lua
+++ lua/voy_console_config/config.lua
@@ -1,6 +1,6 @@
 VOY_GM_Console = {}
-
-VOY_GM_Console_Version = 6
+--
+VOY_GM_Console_Version = 7
 
 VOY_GM_Console_UserGroups = { -- Groups that can edit what's inside the console.
 	["superadmin"] = true,
