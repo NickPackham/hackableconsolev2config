82899c1abbc95890a2be993cb8a65347693881b9
Nick Packham <46549847+NickPackham@users.noreply.github.com>
2020-05-04 17:41:48 -0700

    Update config.lua

--- lua/voy_console_config/config.lua
+++ lua/voy_console_config/config.lua
@@ -1,6 +1,6 @@
 VOY_GM_Console = {}
 
-VOY_GM_Console_Version = 5
+VOY_GM_Console_Version = 6
 
 VOY_GM_Console_UserGroups = { -- Groups that can edit what's inside the console.
 	["superadmin"] = true,
@@ -12,7 +12,7 @@ VOY_GM_Console_Perms = { -- WOS Skills that are required to hack a console. (Onl
 
 VOY_GM_Console_Original = false -- Should the original person who starts the hack be the only one that can collect the Datapad?
 
-VOY_GM_Console_ShouldPerms = false -- Should we use the WiltOS Perms? (Only use if you have alcs.)
+VOY_GM_Console_ShouldPerms = false -- Should we use the WiltOS Skills? (Only use if you have alcs.)
 
 -- Functions
 function VOY_HackCompleted(ply, entity, text) -- Player Who grabbed the Datapad, The Entity, the text the player collected.
